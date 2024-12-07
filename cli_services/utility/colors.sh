891208d21218e8d6e693e73910fe5eb6e2962860
prasad-vamer <prasadkrishnakptp@gmail.com>
2024-12-07 13:16:34 +0900

    feat: Add color utility script for CLI services

---
+++ cli_services/utility/colors.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Define color codes
+BLACK="\033[30m"
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[36m"
+MAGENTA="\033[35m"
+CYAN="\033[36m"
+WHITE="\033[37m"
+RESET="\033[0m"
+
+# Optional: Bold colors
+BOLD_RED="\033[1;31m"
+BOLD_GREEN="\033[1;32m"
+BOLD_YELLOW="\033[1;33m"
+BOLD_BLUE="\033[1;34m"
+BOLD_MAGENTA="\033[1;35m"
+BOLD_CYAN="\033[1;36m"
+BOLD_WHITE="\033[1;37m"
