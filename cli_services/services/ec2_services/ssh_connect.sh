b2d9afc5f27e5415f9f278df66b42be7f288983f
prasad-vamer <prasadkrishnakptp@gmail.com>
2024-12-07 13:16:49 +0900

    feat: Refactor SSH connection script to use region selection utility

--- cli_services/services/ec2_services/ssh_connect.sh
+++ cli_services/services/ec2_services/ssh_connect.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+source utility/colors.sh
+source utility/select_region.sh
 
 # Temporary file paths
 tmp_path="/usr/src/app/cli_services/tmp/"
@@ -6,54 +8,19 @@ array_of_object="arrayOfObject.json"
 obejct_selected="object.json"
 SELECTED_OPTION_OBJECT_PATH=${tmp_path}${obejct_selected}
 
-# Step 1: Prompt user to use default region or choose a region
-DEFAULT_REGION=$(aws configure get region)
-echo -e "\nThe current configured default region is: $DEFAULT_REGION"
-read -p "Do you want to use the default region? (y/yes||n/no): " use_default_region
-
-# Use case statement for robust input handling
-case "${use_default_region,,}" in
-y | yes)
-  REGION=$DEFAULT_REGION
-  echo "Using the default region: $REGION"
-  ;;
-n | no)
-  echo "Retrieving list of AWS regions..."
-
-  # Get the list of all AWS regions
-  ALL_REGIONS=$(aws ec2 describe-regions --query "Regions[].{Name:RegionName}" --output json)
-
-  echo $ALL_REGIONS
-
-  # Save the regions to a temporary file for selection
-  echo "$ALL_REGIONS" >"${tmp_path}${array_of_object}"
-
-  # Use Node.js script to allow user to select a region interactively
-  node interactiveUI/keyValueArrayOfObjectJsonSelect.js -k Name -v Name -f "${array_of_object}" -o "${obejct_selected}"
-
-  # Clean up the temporary file
-  rm "${tmp_path}${array_of_object}"
-
-  # Read the selected region from the JSON file
-  if [ -f "$SELECTED_OPTION_OBJECT_PATH" ]; then
-    REGION=$(cat "$SELECTED_OPTION_OBJECT_PATH" | jq -r '.Name')
-    rm "$SELECTED_OPTION_OBJECT_PATH"
-  else
-    echo "Failed to select a region. Exiting."
-    exit 1
-
-  fi
-  ;;
-*)
-  echo "Invalid input. Please enter 'y/yes' or 'n/no'. Exiting."
+select_region
+# Read the selected region from the JSON file
+if [[ -f "$SELECTED_OPTION_OBJECT_PATH" ]]; then
+  selected_region=$(jq -r '.Name' "$SELECTED_OPTION_OBJECT_PATH") && rm "$SELECTED_OPTION_OBJECT_PATH"
+else
+  echo "Failed to select a region. Exiting."
   exit 1
-  ;;
-esac
+fi
 
-echo -e "\nAccessing Running EC2 instances in region: $REGION"
+echo -e "\nAccessing Running EC2 instances in region: $selected_region"
 
 # Step 2: Get the list of running EC2 instances in the selected region
-RUNNING_INSTANCES_JSON=$(aws ec2 describe-instances --region "$REGION" \
+RUNNING_INSTANCES_JSON=$(aws ec2 describe-instances --region "$selected_region" \
   --filters "Name=instance-state-name,Values=running" \
   --query "Reservations[].Instances[].{InstanceId:InstanceId,PublicIP:PublicIpAddress,Name:Tags[?Key=='Name']|[0].Value}" \
   --output json | jq 'sort_by(.Name)')
@@ -64,11 +31,11 @@ if [ $? -eq 0 ]; then
     echo "$RUNNING_INSTANCES_JSON" | jq .
     echo "$RUNNING_INSTANCES_JSON" >"${tmp_path}${array_of_object}"
   else
-    echo "No running EC2 instances found in region: $REGION."
+    echo "No running EC2 instances found in region: $selected_region."
     exit 1
   fi
 else
-  echo "Failed to retrieve EC2 instance data from region: $REGION."
+  echo "Failed to retrieve EC2 instance data from region: $selected_region."
   exit 1
 fi
 
